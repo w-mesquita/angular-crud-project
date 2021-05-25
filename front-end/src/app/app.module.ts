8e519c0fa77c307f0284d5f9a0f728ce421bdeda
w-mesquita <tom_rast@hotmail.com>
2021-05-25 12:06:31 -0300

    Insert funcionando

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -17,6 +17,12 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -38,7 +44,12 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/components/product/product.model.ts
@@ -0,0 +1,5 @@
+export interface Product {
+    id?: number
+    name: string
+    price: null // VERIFICAR NÃO ACEITAR DEIXAR COMO NUMBER E SETAR NULL
+}
